49a98a12e1cf9c5ec9170c8eb5e88033d80afa9b
hellobbn <clfbbn@gmail.com>
2016-09-03 16:49:28 +0800

    Initial Commit

---
+++ C-Structure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Structure
+//
+//  Created by BBN on 2016/9/3.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
